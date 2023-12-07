b59277c7515486f73001b21b98180caeb1b64c21
Brian Gerkens <67236902+GitCoding123@users.noreply.github.com>
2023-12-06 20:34:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+const thumbnails = document.getElementsByClassName("thumbnail");
+
+const slider = document.getElementById("slider");
+const nextBtn = document.getElementById("slide-right");
+const prevBtn = document.getElementById("slide-left");
+
+// Next Button
+nextBtn.addEventListener("click", () => {
+    scrollAmount = 0;
+    let slideTimer = setInterval(() => {
+        slider.scrollLeft += 10;
+        scrollAmount += 10;
+        if(scrollAmount >= 100) {
+            window.clearInterval(slideTimer);
+        }
+    }, 25);
+});
+
+// Previous Button
+prevBtn.addEventListener("click", () => {
+    scrollAmount = 0;
+    let slideTimer = setInterval(() => {
+        slider.scrollLeft -= 10;
+        scrollAmount += 10;
+        if(scrollAmount >= 100) {
+            window.clearInterval(slideTimer);
+        }
+    }, 25);
+});
+
+
+// // Slider Width Values
+// function sw() {
+//     alert(slider.scrollWidth);
+// }
+
+// function sl() {
+//     alert(slider.scrollLeft);
+// }
+
+// function cw() {
+//     alert(slider.clientWidth);
+// }
+
+// function calc() {
+//     alert(slider.scrollWidth - slider.clientWidth);
+// }
+
+
+// Auto Play Function
+function autoPlay() {
+    if(slider.scrollLeft >= slider.scrollWidth - slider.clientWidth - 1) {
+        slider.scrollLeft = 0;
+    } else {
+        slider.scrollLeft += 1;
+    }
+}
+
+let play = setInterval(autoPlay, 10);
+
+// Pause the slide on hover
+for(let i=0; i<thumbnails.length; i++) {
+    thumbnails[i].addEventListener("mouseover", () => {
+        clearInterval(play);
+    });
+    thumbnails[i].addEventListener("mouseout", () => {
+        return (play = setInterval(autoPlay, 10));
+    });
+}
